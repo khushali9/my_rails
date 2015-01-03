c8be7309733edb5d0c68dbde06c95e0ca50bd2c8
Khushali Desai <khushali.desai@sjsu.edu>
2015-01-02 21:33:46 -0800

    controller added with method edit and update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,18 @@ end
 
 
   def edit
+  @category=Category.find(params[:id])
   end
+  
+  def update
+  @category=Category.find(params[:id])
+  if @category.update_attributes(category_params)
+  redirect_to(:action => 'show', :id => @category.id)
+  else
+  render('index')
+  end
+  
+ end
 
   def delete
   end
