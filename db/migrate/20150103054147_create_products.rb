9dc4f2edf125f05aa080e47ac0a11c0e4a13bbf2
Khushali Desai <khushali.desai@sjsu.edu>
2015-01-02 21:49:02 -0800

    product table created

--- db/migrate/20150103054147_create_products.rb
+++ db/migrate/20150103054147_create_products.rb
@@ -1,8 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps null: false
+t.string :product_name
+t.text :description
+t.float :price 
+t.string :thumburl
+t.references :category
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226220008) do
+ActiveRecord::Schema.define(version: 20150103054147) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
     t.string   "thumburl"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "product_name"
+    t.text     "description"
+    t.float    "price"
+    t.string   "thumburl"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
